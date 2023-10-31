cda8a0eb8ea4135a7a469d9a77dc7a716f6a97ad
Astratos <120133182+EmmanuelDevAtx@users.noreply.github.com>
2023-10-31 09:26:09 -0600

    Add wallet connect provider (#4)
    * change use ether library to use WalletConnect library
    
    Only need change the Ui cause now its hardcode
    
    * change the modal, to provider on layout
    
    * add library forget pino-petry

--- src/app/components/navigation/navigation.tsx
+++ src/app/components/navigation/navigation.tsx
@@ -1,16 +1,75 @@
+import { useWeb3ModalAccount } from "@web3modal/ethers5/react";
 import styles from "./navigation.module.css";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers5/react";
+import { ConnectWallet } from "../connectWallet/connectWallet";
 
 export const Navigation = () => {
+  // 1. Get projectId
+  const projectId = "4a579dd8af908597dde1544a62e922b7";
+
+  // 2. Set chains
+  const mainnet = {
+    chainId: 1,
+    name: "Ethereum",
+    currency: "ETH",
+    explorerUrl: "https://etherscan.io",
+    rpcUrl: "https://cloudflare-eth.com",
+  };
+
+  const mumbai = {
+    chainId: 80001,
+    name: "Mumbai",
+    currency: "Matic",
+    explorerUrl: "https://mumbai.polygonscan.com/",
+    rpcUrl: "https://rpc-mumbai.maticvigil.com",
+  };
+
+  // 3. Create modal
+  const metadata = {
+    name: "Animal care",
+    description: "your mother ",
+    url: "http://localhost:3000/",
+    icons: ["https://avatars.mywebsite.com/"],
+  };
+
+  createWeb3Modal({
+    ethersConfig: defaultConfig({
+      metadata,
+      defaultChainId: 1,
+      enableEIP6963: true,
+      enableInjected: true,
+      enableCoinbase: true,
+      rpcUrl: "...", // used for the Coinbase SDK
+    }),
+    chains: [mainnet, mumbai],
+    projectId,
+  });
+
+  const { address, isConnected } = useWeb3ModalAccount();
+  const router = useRouter();
+  useEffect(() => {
+    console.log('isConnected ', isConnected);
+    console.log('address ', address);
+  }, [isConnected, address]);
+
+  if (address && isConnected) {
+    router.push("/animalHome");
+  }else{
+    router.push("/")
+  }
   return (
     <div className={styles.topnav}>
       <div className={styles.container}>
-        <a className={styles.active} href="#home">
+        <a className={styles.active} href="/">
           Home
         </a>
-        <a href="#news">News</a>
+        <a href="/animalHome">News</a>
         <a href="#contact">Contact</a>
         <a href="#about">About</a>
       </div>
+      <ConnectWallet/>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,13 @@
 "use client";
 import { Navigation } from "./components/navigation/navigation";
 import { ConnectWallet } from "./components/connectWallet/connectWallet";
-import { createWeb3Modal, defaultConfig } from "@web3modal/ethers5/react";
 
 export default function Home() {
-  // 1. Get projectId
-  const projectId = "4a579dd8af908597dde1544a62e922b7";
+ 
 
-  // 2. Set chains
-  const mainnet = {
-    chainId: 1,
-    name: "Ethereum",
-    currency: "ETH",
-    explorerUrl: "https://etherscan.io",
-    rpcUrl: "https://cloudflare-eth.com",
-  };
-
-  const mumbai = {
-    chainId: 80001,
-    name: "Mumbai",
-    currency: "Matic",
-    explorerUrl: "https://mumbai.polygonscan.com/",
-    rpcUrl: "https://rpc-mumbai.maticvigil.com",
-  };
-
-  // 3. Create modal
-  const metadata = {
-    name: "Animal care",
-    description: "your mother ",
-    url: "http://localhost:3000/",
-    icons: ["https://avatars.mywebsite.com/"],
-  };
-
-  createWeb3Modal({
-    ethersConfig: defaultConfig({
-      metadata,
-      defaultChainId: 1,
-      enableEIP6963: true,
-      enableInjected: true,
-      enableCoinbase: true,
-      rpcUrl: "...", // used for the Coinbase SDK
-    }),
-    chains: [mainnet, mumbai],
-    projectId,
-  });
 
   return (
     <main>
-      <Navigation />
-      <ConnectWallet />
     </main>
   );
 }
