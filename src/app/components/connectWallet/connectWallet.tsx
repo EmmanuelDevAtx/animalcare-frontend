be5b7cdba3ba8af40fb87a7490df7836daed3607
EmmanuelDevAtx <120133182+EmmanuelDevAtx@users.noreply.github.com>
2023-10-31 00:23:48 -0600

    change use ether library to use WalletConnect library
    Only need change the Ui cause now its hardcode

--- src/app/components/connectWallet/connectWallet.tsx
+++ src/app/components/connectWallet/connectWallet.tsx
@@ -1,59 +1,21 @@
-import { ethers } from "ethers";
 import { useEffect, useState } from "react";
+import { useWeb3Modal } from '@web3modal/ethers5/react'
+import { useWeb3ModalAccount } from '@web3modal/ethers5/react'
 
 export const ConnectWallet = () => {
   const [current, setAccount] = useState<string>();
-  const [signer, setSigner] = useState<ethers.providers.JsonRpcSigner | null>(
-    null
-  );
-  const [customProvider, setCustomProvider] =
-    useState<ethers.providers.Web3Provider | null>(null);
+  
+  const { open } = useWeb3Modal();
+  const { address, chainId, isConnected } = useWeb3ModalAccount()
 
-  useEffect(() => {
-    const getCustom = async () => {
-      const cp = await new ethers.providers.Web3Provider(window.ethereum);
-      setCustomProvider(cp);
-    };
-    getCustom();
-  }, []);
-
-  async function result() {
-
-    if(!customProvider){
-      return;
-    }
-
-    // MetaMask requires requesting permission to connect users' accounts
-    const accounts = await customProvider.send("eth_requestAccounts", []);
-
-    // The MetaMask plugin also allows signing transactions to
-    // send ether and pay to change state within the blockchain.
-    // For this, you need the account signer...
-    const signer = await customProvider.getSigner();
-    setSigner(signer);
-    console.log("Accounts", accounts);
-    setAccount(accounts[0] as string);
-
-  }
-
-  async function disconect() {
-    window.location.reload();
-    setAccount("");
-  }
-
-  async function showActions() {
-    if(!customProvider){
-      return;
-    }
-    const balance = await customProvider.getBalance(current!);
-    console.log("Balance: ", ethers.utils.formatEther(balance), "ETH");
-  }
   return (
     <div>
-      <h1>current account {current}</h1>
-      <button onClick={result}> collect your walelt</button>;
-      <button onClick={disconect}> disconect</button>;
-      <button onClick={showActions}> showActions</button>;
+      <h1>current account {address}</h1>
+      <h1>is connected: {isConnected}</h1>
+      <h1>chainId {chainId}</h1>
+      <button onClick={()=> open()}> collect your walelt</button>;
+      <button onClick={()=> open({view:'Networks'})}> disconect</button>;
+      {/* <button onClick={showActions}> showActions</button>; */}
     </div>
   );
 };
