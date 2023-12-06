f7b1217b4b23fa173104194db8e7e1e19fd020ef
Astratos <120133182+EmmanuelDevAtx@users.noreply.github.com>
2023-12-05 20:28:27 -0600

    add hook (#12)

---
+++ src/app/hooks/screenInfo.tsx
@@ -0,0 +1,49 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+interface ScreenInformationContextInterface {
+  screenHeight: number;
+  screenWidth: number;
+  isSmall: boolean;
+}
+
+const ScreenInformationContext =
+  createContext<ScreenInformationContextInterface>({
+    screenHeight: 0,
+    screenWidth: 0,
+    isSmall: false,
+  });
+
+export const ScreenInformationProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [screenHeight, setScreenHeight] = useState<number>(0);
+  const [screenWidth, setScreenWidth] = useState<number>(0);
+  const isSmall = screenWidth >= 600;
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setScreenHeight(window.innerHeight);
+      setScreenWidth(window.innerWidth);
+    };
+    
+    window.addEventListener('resize', handleResize);
+    setScreenHeight(window.innerHeight);
+    setScreenWidth(window.innerWidth);
+    
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <ScreenInformationContext.Provider
+      value={{ screenHeight, screenWidth, isSmall }}
+    >
+      {children}
+    </ScreenInformationContext.Provider>
+  );
+};
+
+export const UseScreenInfo = () => {
+  return useContext(ScreenInformationContext);
+};
