fd8e3a1910454a9d00754183493aa16f90f696f5
Astratos <120133182+EmmanuelDevAtx@users.noreply.github.com>
2023-12-05 20:27:49 -0600

    add hook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./styles/styles.css";
 import { Navigation } from "./components/navigation/navigation";
 import { ContractProvider } from "./hooks/contractActions";
 import { TestThreeJs } from "./components/custom/testThree";
+import { ScreenInformationProvider } from "./hooks/screenInfo";
 
 export default function RootLayout({
   children,
@@ -14,15 +15,19 @@ export default function RootLayout({
   return (
     <html lang="es" className="dark">
       <body>
-        <ContractProvider>
-          <div className="bg-fixed bg-cover h-screen bg-black background-custom" style={{zIndex: -1}} />
-          <TestThreeJs />
-          <div style={{zIndex:2}}>
-          <Navigation />
-          {children}
-
-          </div>
-        </ContractProvider>
+        <ScreenInformationProvider>
+          <ContractProvider>
+            <div
+              className="bg-fixed bg-cover h-screen bg-black background-custom"
+              style={{ zIndex: -1 }}
+            />
+            <TestThreeJs />
+            <div style={{ zIndex: 2 }}>
+              <Navigation />
+              {children}
+            </div>
+          </ContractProvider>
+        </ScreenInformationProvider>
       </body>
     </html>
   );
